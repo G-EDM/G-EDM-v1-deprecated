f7cca22a50c85e5bff2dbd4971ac784e13534324
crypt <HnyBdgrDGAS@gmail.com>
2023-10-05 16:37:33 +0200

    licensing

--- Software/G-RBL/lib/GEDM/gedm_main.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "tft_display/ili9341_tft.h"
-
-
-void start_services( void );
-void setup_adc_channels( void );
-void set_z_microsteps( int microsteps );
--- Software/G-RBL/lib/GEDM/tft_display/ili9341_config.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#define REPEAT_CAL false              // if set to true display calibration is forced to repeat
-
-#define DISABLE_TFT_CALIBRATION false // if no display is attached the first time calibration will block everything
-
-#define TOUCH_CALIBRATION_FILE "/touch_calibration_data"
-
-
